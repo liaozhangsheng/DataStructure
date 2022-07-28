65017e2f7dd8dd753a7c57ba1bc6d4767510eaf3
liaozhangsheng <liaozhangsheng@163.com>
2022-07-28 10:21:38 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+#define MAXSIZE 100
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+typedef int Status;
+typedef char ElemType;
+
+typedef struct
+{
+    char ISBN[20];
+    char name[50];
+    float price;
+} elem;
+
+typedef struct
+{
+    ElemType *elem;
+    int length;
+    int listsize;
+} SqList;
+
+Status InitList_Sq(SqList &L)
+{
+    L.elem = new ElemType[MAXSIZE];
+    if (!L.elem)
+    {
+        exit(OVERFLOW);
+    }
+    L.length = 0;
+    L.listsize = MAXSIZE;
+    return OK;
+}
+
+void DestoryList(SqList &L)
+{
+    if (L.elem)
+        delete L.elem;
+}
+
+void ClearList(SqList &L)
+{
+    L.length = 0;
+}
+
+Status GetLength(SqList L)
+{
+    return L.length;
+}
+
+Status IsEmpty(SqList L)
+{
+    return L.length == 0 ? 1 : 0;
+}
+
+Status GetElem(SqList L, Status i, ElemType &e)
+{
+    if (i < 1 || i > L.length)
+        return ERROR;
+    e = L.elem[i - 1];
+    return OK;
+}
+
+Status LocateElem(SqList &L, Status i, ElemType e)
+{
+    if (L.elem[i] == e)
+        return i + 1;
+    return 0;
+}
+
+Status ListInsert_Sq(SqList &L, Status i, ElemType e)
+{
+    if (i < 1 || i > L.length + 1)
+        return ERROR;
+    if (L.length == MAXSIZE)
+        return ERROR;
+    for (Status j = L.length - 1; j >= i - 1; j--)
+    {
+        L.elem[j + 1] = L.elem[j];
+    }
+    L.elem[i - 1] = e;
+    L.length++;
+    return OK;
+}
+
+Status ListDelete_Sq(SqList &L,Status i,ElemType e){
+    if (i<1||i>L.length) return ERROR;
+    for (int j = i; j <= L.length-1; j++)
+    {
+        L.elem[j-1]=L.elem[j];
+    }
+    L.length--;    
+    return OK;
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
